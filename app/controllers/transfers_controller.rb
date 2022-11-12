830f33ee79688392f5cc04dfa329bb4ed4b97787
Chrispaix KAZE <kchrispaixs@gmail.com>
2022-11-12 18:17:03 +0200

    Add categories and transfers controllers

---
+++ app/controllers/transfers_controller.rb
@@ -0,0 +1,20 @@
+class TransfersController < ApplicationController
+    def index
+      @category = Category.find_by(id: params[:id])
+    end
+
+    def ancient
+      @category = Category.find_by(id: params[:id])
+    end
+
+    def new
+      @transfer_id = params[:id]
+    end
+
+    def create
+        @transfer = Transfer.create(name: params[:name], amount: params[:amount],user_id: current_user.id)
+        
+        JoinCategoryWithTransfer.create(category_id: params[:transfer_id], transfer_id: @transfer.id)
+        redirect_to "/categories/#{params[:transfer_id]}/transfers"
+    end
+end
