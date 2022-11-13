7b75b0ff883e5f630d80d35716f68530a9245231
Chrispaix KAZE <kchrispaixs@gmail.com>
2022-11-13 12:43:23 +0200

    Add category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,4 @@
-class Category < ApplicationRecord
+class Category < ApplicationRecord    
     belongs_to :user, dependent: :destroy
     has_many :join_categories_with_transfers, dependent: :destroy
     has_many :transfers, through: :join_categories_with_transfers
