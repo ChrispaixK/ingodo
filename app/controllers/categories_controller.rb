5b5ff5e1c6340f04159a6d427395d5055b425c01
Chrispaix KAZE <kchrispaixs@gmail.com>
2022-11-16 19:32:20 +0200

    Refactor and Use strong params in controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,10 +3,32 @@ class CategoriesController < ApplicationController
     @user_categories = Category.where(user_id: current_user.id)
   end
 
-  def new; end
+  def new; 
+    @category = Category.new
+  end
 
   def create
-    Category.create(name: params[:name], icon: params[:icon_link], user_id: params[:user_id])
-    redirect_to '/'
+    # Category.create(name: params[:name], icon: params[:icon], user_id: params[:user_id])
+    # redirect_to '/categories'
+    @category = Category.new(category_params)
+    @category.user = current_user
+    
+    respond_to do |format|
+      format.html do
+        if @category.save
+          flash[:notice] = 'Category created successfully'
+          redirect_to categories_path
+        else
+          flash[:error] = 'Category not created'
+          redirect_to categories_path
+        end
+      end
+    end
+  end
+
+  private 
+
+  def category_params
+    params.require(:category).permit(:name, :icon)
   end
 end
