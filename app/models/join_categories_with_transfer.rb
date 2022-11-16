7513729f6e9c5febb1f3a73a3d21720d4d35a8bd
Chrispaix KAZE <kchrispaixs@gmail.com>
2022-11-16 19:34:38 +0200

    Add other changes required and refactor MVC and routes

--- app/models/join_categories_with_transfer.rb
+++ app/models/join_categories_with_transfer.rb
@@ -1,7 +1,4 @@
 class JoinCategoriesWithTransfer < ApplicationRecord
   belongs_to :category
   belongs_to :transfer
-
-  validates :category_id, presence: true
-  validates :transfer_id, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,9 @@ Rails.application.routes.draw do
   #devise_for :users
   devise_for :users, sign_out_via: %i[get post]
   # Defines the root path route ("/")
-  root "categories#index"
-  get '/categories/new', to: 'categories#new'
-  post 'categories/create', to: 'categories#create'
-  get 'categories/:id/transfers', to: 'transfers#index'
-  get 'categories/:id/transfers/new', to: 'transfers#new'
-  post '/categories/:id/transfers/create', to: 'transfers#create'
+  root "home#index"
+  resources :categories do 
+    resources :transfers
+  end
 
 end
