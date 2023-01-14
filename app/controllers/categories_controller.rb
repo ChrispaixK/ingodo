2a9775ff0fe3b5660c2971049fcf207c4a43b161
Chrispaix KAZE <kchrispaixs@gmail.com>
2023-01-14 23:59:03 +0200

    debug categories controllers

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,33 +1,33 @@
-class CategoriesController < ApplicationController
-  def index
-    @user_categories = Category.where(user_id: current_user.id)
-  end
+# class CategoriesController < ApplicationController
+#   def index
+#     @user_categories = Category.where(user_id: current_user.id)
+#   end
 
-  def new
-    @category = Category.new
-  end
+#   def new
+#     @category = Category.new
+#   end
 
-  def create
-    # Category.create(name: params[:name], icon: params[:icon], user_id: params[:user_id])
-    # redirect_to '/categories'
-    @category = Category.new(category_params)
-    @category.user = current_user
+#   def create
+#     # Category.create(name: params[:name], icon: params[:icon], user_id: params[:user_id])
+#     # redirect_to '/categories'
+#     @category = Category.new(category_params)
+#     @category.user = current_user
 
-    respond_to do |format|
-      format.html do
-        if @category.save
-          flash[:notice] = 'Category created successfully'
-        else
-          flash[:alert] = 'Category not created Try Again!'
-        end
-      end
-    end
-    redirect_to categories_path
-  end
+#     respond_to do |format|
+#       format.html do
+#         if @category.save
+#           flash[:notice] = 'Category created successfully'
+#         else
+#           flash[:alert] = 'Category not created Try Again!'
+#         end
+#       end
+#     end
+#     redirect_to categories_path
+#   end
 
-  private
+#   private
 
-  def category_params
-    params.require(:category).permit(:name, :icon)
-  end
-end
+#   def category_params
+#     params.require(:category).permit(:name, :icon)
+#   end
+# end
