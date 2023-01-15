e828f28d57b03b7c177b64b31bf98e2c1819194d
Chrispaix KAZE <kchrispaixs@gmail.com>
2023-01-15 21:23:48 +0200

    debug

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
-class Category < ApplicationRecord
-  belongs_to :user, dependent: :destroy
-  has_many :join_categories_with_transfers, dependent: :destroy
-  has_many :transfers, through: :join_categories_with_transfers
+# class Category < ApplicationRecord
+#   belongs_to :user, dependent: :destroy
+#   has_many :join_categories_with_transfers, dependent: :destroy
+#   has_many :transfers, through: :join_categories_with_transfers
 
-  validates :name, :icon, presence: true
-end
+#   validates :name, :icon, presence: true
+# end
